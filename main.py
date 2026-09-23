ed4436daa90dd08853a4789aeac88fc7a64e0668
formypad8-code <formypad8@gmail.com>
2026-09-23 07:30:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+ftimport json
 import os
 import requests
 from groq import Groq
@@ -41,7 +41,7 @@ Rules:
 
 chat_completion = client.chat.completions.create(
     messages=[{"role": "user", "content": prompt}],
-    model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
     temperature=0.5,
 )
 
